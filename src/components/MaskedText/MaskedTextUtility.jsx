e8de1d25a3bcbac383578c1949a6272653e89a62
Varun Kumar <varungecsiwan@gmail.com>
2025-06-24 15:26:36 +0530

    more code added

--- src/components/MaskedText/MaskedTextUtility.jsx
+++ src/components/MaskedText/MaskedTextUtility.jsx
@@ -1,5 +1,5 @@
 export function getAllCharacters(word, usedLetters) {
-    const usedLetters = usedLetters.map((ele) => ele.toUpperCase());
+    usedLetters = usedLetters.map((ele) => ele.toUpperCase());
     const guessedLetters = new Set(usedLetters);
     const characters = word.toUpperCase().split('').map((char) => {
         if (guessedLetters.has(char)) {
--- src/pages/PlayGame/PlayGame.jsx
+++ src/pages/PlayGame/PlayGame.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import MaskedText from "../../components/MaskedText/MaskedText";
 
 function PlayGame()
 {
     return(
         <div>
             <h1>Play Game</h1>
+            <MaskedText text="humble" usedLetters={['b','e']}/>
             <Link to={'/start'}>Start Game</Link>
         </div>
     );
