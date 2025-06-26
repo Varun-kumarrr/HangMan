5837bcc4f88fc48ed6830b87a9d433b4b492ddde
Varun Kumar <varungecsiwan@gmail.com>
2025-06-26 14:43:13 +0530

    add css in buttons

--- src/components/LetterButtons/LetterButtons.jsx
+++ src/components/LetterButtons/LetterButtons.jsx
@@ -1,37 +1,40 @@
 const ALPHABETS = new Array(26).fill('').map((e, index) => String.fromCharCode(65 + index));
 
-function LetterButtons({usedLetters,onLetterClick}) {
+function LetterButtons({ text, usedLetters, onLetterClick }) {
     const selectedLetters = new Set(usedLetters.join('').toUpperCase().split(''));
-    const buttonStyle = function(letter)
+    const originalCharacters = new Set(text.toUpperCase().split(''))
+
+    const buttonStyle = function (letter)
     {
         if(selectedLetters.has(letter))
         {
-            return 'bg-red-500 border border-red-700 cursor-pointer hover:bg-red-700'
+            return `${originalCharacters.has(letter) ? 'bg-green-500 border-green-700' : 'bg-red-500 border-red-700'}`;
         }
-        else {
-            return 'bg-blue-500 border border-blue-700 cursor-pointer hover:bg-blue-700';
+        else
+        {
+            return 'bg-blue-500 border-blue-700 hover:bg-blue-700'
         }
     }
 
-    const handleClick = function (event)
-    {
+
+    const handleClick = function (event) {
         const character = event.target.value;
         onLetterClick?.(character)
     }
 
     const buttons = ALPHABETS.map((letter) => {
         return (
-            <button 
-            value={letter}
-            disabled = {selectedLetters.has(letter)}
-            onClick={handleClick}
-            className={`h-12 w-12 m-1 rounded-md focus:outline-none text-white ${buttonStyle(letter)} `} 
-            key={`button-${letter}`}>
+            <button
+                value={letter}
+                disabled={selectedLetters.has(letter)}
+                onClick={handleClick}
+                className={`h-12 w-12 m-1 rounded-md focus:outline-none text-white ${buttonStyle(letter)} `}
+                key={`button-${letter}`}>
                 {letter}
             </button>
         );
     });
-    
+
 
     return (
         <>
